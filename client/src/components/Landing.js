056581e9990b2104a10d169c906c07f6d742efd3
sasa-geric <sasa.geric@hpe.com>
2020-11-30 15:20:28 +0100

    added billing and lient side app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+
+export class Landing extends Component {
+    render() {
+        return(
+            <div style={{ textAlign: 'center'}}>
+                <h1>Emaily!</h1>
+                Collect feedback from your users
+            </div>
+        )
+    }
+}
--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     cookieKey: process.env.COOKIE_KEY,
-    googleRedirectURI: 'https://nameless-ridge-19551.herokuapp.com'
+    googleRedirectURI: 'https://nameless-ridge-19551.herokuapp.com',
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY
 }
